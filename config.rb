9c029d73a3a1f6604084c2410a38aac7dfd9dbf9
Kevin Manssat <kevin.manssat@hotmail.fr>
2017-11-13 12:45:41 +0100

    Semaine intensive structure
    Semaine intensive, structure

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'css'
+images_dir = 'img'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = false
+output_style = :neasted
