7a5a0858f42afc3a2811b4c6b7b00bc53e4dc2bc
Kevin Manssat <kevin.manssat@hotmail.fr>
2017-11-15 10:13:57 +0100

    15/11 10h

--- js/app.js
+++ js/app.js
@@ -1,18 +1,33 @@
 var header = document.querySelector(".header");
 var headerH = header.offsetHeight;
 var navbar = document.querySelector(".navbar");
+var toShow = document.querySelectorAll(".toShow");
 
-window.addEventListener('scroll', function(){
-    if (scrollY >= (headerH/2)) {
+var stickynav = function(){
+    if (scrollY > (headerH/2)) {
         navbar.classList.add("fixed");
     }else{
         navbar.classList.remove("fixed")
     }
-    if(scrollY >= headerH && navbar.classList.contains("fixed")){
+    if(scrollY > headerH && navbar.classList.contains("fixed")){
         navbar.classList.add("--sticky");
     }else{
         navbar.classList.remove("--sticky");
     }
+}
 
+var spyshow = function(){
+    for (let i = 0; i < toShow.length; i++) {
+        var element = toShow[i];
+        if (element.offsetTop < scrollY + (window.innerHeight /2)) {
+            element.classList.add("showed");
+        }
+    }
+}
 
+window.addEventListener('scroll', function(){
+    
+    stickynav();
+    spyshow();
+   
 });
